78e4ee60966a2502899218c93f298777f72f843b
junayet <muhammadjunayetmaruf@gmail.com>
2023-12-20 21:38:29 +0600

    task Sphere has started

---
+++ src/MyRouts.jsx
@@ -0,0 +1,20 @@
+// import React from 'react';
+
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "./assets/MainLayout/MainLayout";
+import Home from "./Home/Home";
+
+const MyRouts = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    },
+]);
+
+export default MyRouts;
